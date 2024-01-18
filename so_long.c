255ac500019a2fd7d2bcc6291193e469eebe4ec4
SaltyChickenLeg <merijnjong1@gmail.com>
2024-01-18 18:21:05 +0100

    getting closer to movement

--- so_long.c
+++ so_long.c
@@ -6,29 +6,52 @@
 /*   By: mjong <mjong@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 14:52:55 by mjong             #+#    #+#             */
-/*   Updated: 2024/01/18 16:32:11 by mjong            ###   ########.fr       */
+/*   Updated: 2024/01/18 18:19:45 by mjong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void my_keyhook(mlx_key_data_t keydata)
+typedef struct
 {
-	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS)
-	{
-		puts("EXIT");
-		exit(EXIT_SUCCESS);
-	}
+	int	xpos;
+	int	ypos;
+	int	width;
+	int	height;
+} Player;
+
+void move_rectangle(Player *rect, int xdir, int ydir)
+{
+    rect->xpos += xdir;
+    rect->ypos += ydir;
 }
 
-// typedef void (*mlx_closefunc)
-// {
-// 	if (keydata.key == MLX_KEY_X && keydata.action == MLX_PRESS)
-// 	{
-// 		puts("EXIT_SUCCESS");
-// 		exit(EXIT_SUCCESS);
-// 	}
-// }
+void ft_hooks(mlx_key_data_t keydata, Player *rect)
+{
+	if (keydata.action == MLX_PRESS || keydata.action == MLX_REPEAT)
+	{
+        int xdir;
+		int ydir;
+		
+		xdir = 0;
+		ydir = 0;
+		if (keydata.key == MLX_KEY_ESCAPE)
+		{
+			puts("EXIT");
+			exit(EXIT_SUCCESS);
+		}
+        if (keydata.key == MLX_KEY_W)
+			ydir = -10;
+        if (keydata.key == MLX_KEY_A)
+            xdir = -10;
+        if (keydata.key == MLX_KEY_S)
+            ydir = 10;
+        if (keydata.key == MLX_KEY_D)
+            xdir = 10;
+        move_rectangle(rect, xdir, ydir);
+    }
+	printf("Key pressed: %d\n", keydata.key);
+}
 
 mlx_image_t *ft_draw_rect(mlx_t *mlx, uint32_t width, uint32_t height, uint32_t color)
 {
@@ -58,26 +81,13 @@ mlx_image_t *ft_draw_rect(mlx_t *mlx, uint32_t width, uint32_t height, uint32_t 
 int32_t	main(void)
 {
 	mlx_t		*mlx;
-	mlx_image_t	*img;
 	mlx_image_t	*rect;
-	uint32_t	x;
-	uint32_t	y;
+	Player player = {380, 200, 30, 30};
 
 	mlx = mlx_init(800, 400, "kroeg", true);
-	img = mlx_new_image(mlx, 800, 400);
-	rect = ft_draw_rect(mlx, 30, 30, 0x70BFFF);
-	x = 380;
-	y = 200;
-	while (x++ <= 420)
-		mlx_put_pixel(img, x, y, 0xffffff);
-	x = 400;
-	y = 180;
-	while (y++ <= 220)
-		mlx_put_pixel(img, x, y, 0xffffff);
-	mlx_image_to_window(mlx, img, 0, 0);
-	mlx_image_to_window(mlx, rect, 0, 0);
-	mlx_key_hook(mlx, (void *)&my_keyhook, rect);
-	// mlx_loop_hook(mlx, (void *)&my_keyhook, mlx);
+	rect = ft_draw_rect(mlx, player.width, player.height, 0x70BFFF);
+	mlx_image_to_window(mlx, rect, player.xpos, player.ypos);
+	mlx_loop_hook(mlx, (void *)&ft_hooks, &player);
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
 	return (EXIT_SUCCESS);
