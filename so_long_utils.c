e68a199d5c0711b87f0acdd29e48a0963f780d36
SaltyChickenLeg <merijnjong1@gmail.com>
2024-03-06 18:15:01 +0100

    almost done

--- so_long_utils.c
+++ so_long_utils.c
@@ -6,7 +6,7 @@
 /*   By: mjong <mjong@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 17:53:38 by mjong             #+#    #+#             */
-/*   Updated: 2024/02/20 17:45:28 by mjong            ###   ########.fr       */
+/*   Updated: 2024/03/06 18:14:43 by mjong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,23 @@
 
 int	ft_mapcheck(t_game *game)
 {
+	// int j;
+	// int i;
+	// int answer;
+
+	// j = 0;
+	// i = 0;
+	// while (game->two_d_map[j] != NULL && game->two_d_map[j][0] != '\0')
+    // 	j++;
+	// if (j > 0)
+	// {
+    // 	while (game->two_d_map[0][i] != '\0')
+    //  	   i++;
+	// }
+	// answer = i * j;
+	// ft_printf("%d", answer);
+	// if (answer == '\0')
+	// 	return (0);
 	if (game->colnum < 1 || game->exinum != 1 || game->planum != 1)
 		return (0);
 	else
@@ -22,6 +39,14 @@ int	ft_mapcheck(t_game *game)
 
 void ft_exitgame(t_game *game)
 {
+	int	j;
+
+	j = 0;
+	while (game->two_d_map[j])
+	{
+		free(game->two_d_map[j]);
+		j++;
+	}
 	free(game->two_d_map);
-	mlx_terminate(game->mlx);
+	mlx_close_window(game->mlx);
 }
--- so_longbackup.c
+++ so_longbackup.c
@@ -6,13 +6,142 @@
 /*   By: mjong <mjong@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 16:20:51 by mjong             #+#    #+#             */
-/*   Updated: 2024/02/07 14:47:03 by mjong            ###   ########.fr       */
+/*   Updated: 2024/03/06 14:06:35 by mjong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	ft_makeimg(struct t_data *game)
+void	move_player2(t_game *game, uint32_t xdir, uint32_t ydir)
+{
+	uint32_t	prevxdir;
+	uint32_t	prevydir;
+
+	prevxdir = game->xpos;
+	prevydir = game->ypos;
+	game->xpos += xdir;
+	game->ypos += ydir;
+	printf("xpos: %d\n", game->xpos);
+	printf("ypos: %d\n", game->ypos);
+	mlx_image_to_window(game->mlx, game->ffloor, prevxdir, prevydir);
+	mlx_image_to_window(game->mlx, game->player, game->xpos, game->ypos);
+}
+
+void	move_player(t_game *game, uint32_t xdir, uint32_t ydir)
+{
+	uint32_t	prevxdir;
+	uint32_t	prevydir;
+
+	prevxdir = game->xpos;
+	prevydir = game->ypos;
+	int x = (game->xpos + xdir) / 100;
+	int y = (game->ypos + ydir) / 100;
+	if (game->two_d_map[y][x] != '1')
+	{
+		if (game->two_d_map[y][x] == 'C')
+			game->colnum--;
+		if (game->two_d_map[y][x] == 'E' && game->colnum == 0)
+			ft_exitgame(game);
+		move_player2(game, xdir, ydir);
+	}
+}
+
+void	ft_hooks(mlx_key_data_t keydata, t_game *game)
+{
+	uint32_t	xdir;
+	uint32_t	ydir;
+
+	xdir = 0;
+	ydir = 0;
+	if (keydata.action == MLX_PRESS || keydata.action == MLX_REPEAT)
+	{
+		if (keydata.key == MLX_KEY_ESCAPE)
+			ft_exitgame(game);
+		if (keydata.key == MLX_KEY_W)
+			ydir = -100;
+		if (keydata.key == MLX_KEY_A)
+			xdir = -100;
+		if (keydata.key == MLX_KEY_S)
+			ydir = 100;
+		if (keydata.key == MLX_KEY_D)
+			xdir = 100;
+		if (xdir || ydir)
+			move_player(game, xdir, ydir);
+	}
+}
+
+void	ft_makemap(char indicator, t_game *game, int x, int y)
+{
+	x *= 100;
+	y *= 100;
+	if (indicator == 'C')
+	{
+		mlx_image_to_window(game->mlx, game->collectible, x, y);
+		game->colnum += 1;
+	}
+	if (indicator == 'E')
+	{
+		mlx_image_to_window(game->mlx, game->eexit, x, y);
+		game->exinum += 1;
+	}
+	if (indicator == 'P')
+	{
+		mlx_image_to_window(game->mlx, game->player, x, y);
+		game->planum += 1;
+		game->xpos = x;
+		game->ypos = y;
+	}
+	if (indicator == '0')
+		mlx_image_to_window(game->mlx, game->ffloor, x, y);
+	if (indicator == '1')
+		mlx_image_to_window(game->mlx, game->wall, x, y);
+}
+
+int display_map(t_game *game)
+{
+	int x = 0;
+	int y =	0;
+	while (game->two_d_map[0][game->width])
+		game->width++;
+	// printf("H: %d - W: %d\n", game->ylength, game->width);
+	game->mlx = mlx_init(game->width * 100, game->ylength * 100, "starwars", true);
+	ft_makeimg(game);
+	while (y != game->ylength && x != game->width)
+	{
+		while (x != game->width)
+		{
+			ft_makemap(game->two_d_map[y][x], game, x, y);
+			x++;
+		}
+		y++;
+		x = 0;
+	}
+	return (1);
+}
+
+char	*ft_filetomap(t_game *game)
+{
+	int		fd;
+	char	*map;
+	char	*temp;
+
+	fd = open("swmap.ber", O_RDONLY);
+	map = NULL;
+	if (fd < 0)
+		return (NULL);
+	while (1)
+	{
+		temp = get_next_line(fd);
+		if (!temp)
+			break ;
+		game->ylength++;
+		map = ft_strjoin2(map, temp);
+	}
+	close(fd);
+	return (map);
+}
+
+void	ft_makeimg(t_game *game)
 {
 	mlx_texture_t	*c;
 	mlx_texture_t	*e;
@@ -32,113 +161,59 @@ void	ft_makeimg(struct t_data *game)
 	game->wall = mlx_texture_to_image(game->mlx, w);
 }
 
-void	move_player(struct t_data *game, uint32_t xdir, uint32_t ydir)
+void	init(t_game *game)
 {
-	uint32_t	prevxdir;
-	uint32_t	prevydir;
-
-	prevxdir = game->xpos;
-	prevydir = game->ypos;
-	mlx_image_to_window(game->mlx, game->ffloor, prevxdir, prevydir);
-	game->xpos += xdir;
-	game->ypos += ydir;
-	printf("xpos: %d\n", game->xpos);
-	printf("ypos: %d\n", game->ypos);
-	// if (game->xpos != game->wall || game->ypos != game->wall)
-		mlx_image_to_window(game->mlx, game->player, game->xpos, game->ypos);
+	// game->xpos = 100;
+	// game->ypos = 100;
+	game->width = 0;
+	game->height = 0;
+	game->colnum = 0;
+	game->exinum = 0;
+	game->planum = 0;
+	game->ylength = 0;
+	game->mlx = NULL;
+	game->collectible = NULL;
+	game->eexit = NULL;
+	game->ffloor = NULL;
+	game->player = NULL;
+	game->wall = NULL;
+	game->two_d_map = NULL;	
 }
 
-void	ft_hooks(mlx_key_data_t keydata, struct t_data *game)
-{
-	uint32_t	xdir;
-	uint32_t	ydir;
-
-	xdir = 0;
-	ydir = 0;
-	if (keydata.action == MLX_PRESS || keydata.action == MLX_REPEAT)
-	{
-		if (keydata.key == MLX_KEY_ESCAPE)
-			mlx_close_window(game->mlx);
-		if (keydata.key == MLX_KEY_W)
-			ydir = -100;
-		if (keydata.key == MLX_KEY_A)
-			xdir = -100;
-		if (keydata.key == MLX_KEY_S)
-			ydir = 100;
-		if (keydata.key == MLX_KEY_D)
-			xdir = 100;
-		move_player(game, xdir, ydir);
-	}
-}
-
-void	ft_makemap(char *map, struct t_data *game, int x, int y)
-{
-	while (*map != '\n' && *map != '\0')
-	{
-		if (*map == 'C')
-		{
-			mlx_image_to_window(game->mlx, game->collectible, x, y);
-			game->colnum += 1;
-		}
-		if (*map == 'E')
-		{
-			mlx_image_to_window(game->mlx, game->eexit, x, y);
-			game->exinum += 1;
-		}
-		if (*map == 'P')
-		{
-			mlx_image_to_window(game->mlx, game->player, x, y);
-			game->planum += 1;
-		}
-		if (*map == '0')
-			mlx_image_to_window(game->mlx, game->ffloor, x, y);
-		if (*map == '1')
-			mlx_image_to_window(game->mlx, game->wall, x, y);
-		map++;
-		x += 100;
-	}
-}
-
-void	ft_filetomap(struct t_data *game)
-{
-	int		x;
-	int		y;
-	int		fd;
-	int		check;
-	char	*map;
-
-	check = 0;
-	y = 0;
-	fd = open("swmap.ber", O_RDONLY);
-	while (!check)
-	{
-		map = get_next_line(fd);
-		if (!map || map[0] == '\0')
-		{
-			check = 1;
-			free(map);
-			break ;
-		}
-		x = 0;
-		ft_makemap(map, game, x, y);
-		y += 100;
-		free(map);
-	}
-	close(fd);
-}
 
 int32_t	main(void)
 {
-	mlx_t	*mlx;
-	mlx = mlx_init(2100, 1400, "starwars", true);
-	Game game = {100, 100, 100, 100, 0, 0, 0, mlx, NULL, NULL, NULL, NULL, NULL};
+	t_game	game;
+	char	*map;
 
-	ft_makeimg(&game);
-	ft_filetomap(&game);
-	if (ft_mapcheck(game) == 0)
-		mlx_close_window(game.mlx);
-	mlx_key_hook(mlx, (void *)&ft_hooks, &game);
-	mlx_loop(mlx);
-	mlx_terminate(mlx);
-	return (EXIT_SUCCESS);
+	init(&game);
+
+	map = ft_filetomap(&game);
+	game.two_d_map = ft_split(map, '\n');
+
+	// GRID CHECK
+	int i = 0;
+	int o = 0;
+	printf("\n");
+	while (i < 4)
+	{
+		while (o < 6)
+		{
+			printf("%c", game.two_d_map[i][o]);
+			o++;
+		}
+		o = 0;
+		i++;
+		printf("\n");
+	}
+	// END CHECK
+	
+	display_map(&game);
+	if (ft_mapcheck(&game) == 0)
+		ft_exitgame(&game);
+	mlx_key_hook(game.mlx, (void *)&ft_hooks, &game);
+	mlx_loop(game.mlx);
+	free(game.two_d_map);
+	mlx_terminate(game.mlx);
+	return (0);
 }
